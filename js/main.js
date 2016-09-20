4a4a598cbc5d71c92cacf99ff27d6f69048ba3d1
jocelynlozano <jocelynlozanocarbajal@gmail.com>
2016-09-20 10:57:09 -0500

    tag2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
 window.addEventListener("load", function() {
-		var boton=document.getElementById("boton");
-		boton.addEventListener("click",function(){
-			var txt=document.getElementById("txt").value;
-			var mensaje=document.createElement("p");
-			mensaje.innerText=txt;
-			var contenedor=document.getElementById("contenedor");
-			contenedor.insertBefore(mensaje,contenedor.childNodes[0]);
+	var boton=document.getElementById("boton");
+	var textArea=document.getElementById("texto");
+	var contador=document.getElementById("contador");
+	var caracteres=contador.innerHTML =140;
+		
+	boton.addEventListener("click",function(e){
+		e.preventDefault();
+		agregarMensaje(textArea.value);
+		textArea.value = "";
+		caracteres = 140;
+		boton.disabled = true;
 	});
-
+		
+	function agregarMensaje(txt){
+		var parrafo = document.createElement("p");
+		parrafo.innerHTML = txt;
+		var mensaje = document.getElementById("mensaje");
+		mensaje.insertBefore(parrafo, imprimir.childNodes[0]).classList.add("box");
+	}
 });
+
+
+
+textArea.addEventListener("keydown",function(){
+		boton.disabled=false;
+});
+
+var longitud = textArea.value.length;
+if (longitud <= caracteres) {
+	contador.innerHTML = caracteres - longitud;
+} else {
+	contador.innerHTML=caracteres - longitud;
+};
